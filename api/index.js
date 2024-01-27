9e71e05620b65e8f9a06988f416d838e1da432db
Min Paing <minmyatpaing64@gmail.com>
2024-01-27 18:28:37 +0000

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to Mongo");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running at port 3000");
-})
+  console.log("Server is running at port 3000");
+});
