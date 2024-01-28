f0315bd3b53f3dc8bc04864dbb6ddfe9d8c07379
Min Paing <minmyatpaing64@gmail.com>
2024-01-28 17:04:31 +0000

    created error handling middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcryptjs from "bcryptjs";
 
 import User from "../models/user.model.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -13,6 +13,6 @@ export const signup = async (req, res) => {
 
     res.status(201).json("User created!");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
