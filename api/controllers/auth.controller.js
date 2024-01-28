3748cd93ba08094bbb362fee2e790c724732e895
Min Paing <minmyatpaing64@gmail.com>
2024-01-28 18:46:51 +0000

    added sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
@@ -15,6 +16,33 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json("User created!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const verifiedUser = await User.findOne({ email });
+    if (!verifiedUser) {
+      errorHandler(404, "User not found!");
+    }
+
+    const verifiedPassword = bcryptjs.compareSync(
+      password,
+      verifiedUser.password
+    );
+    if (!verifiedPassword) {
+      errorHandler(401, "Incorrect credentials!");
+    }
+
+    const token = jwt.sign({ id: verifiedUser._id }, process.env.JWT_SECRET);
+    const { password: _, ...rest } = verifiedUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
