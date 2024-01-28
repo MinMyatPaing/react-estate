edc333fe685cba327cd62ecd92bba690ae4c1b96
Min Paing <minmyatpaing64@gmail.com>
2024-01-28 18:27:19 +0000

    completed sign up page and its functions

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,16 @@
 import bcryptjs from "bcryptjs";
 
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-
   try {
+    if (!username && !email && !password) {
+      errorHandler(401, "Invalid Input!");
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
 
